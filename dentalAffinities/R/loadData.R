48b61d7684b60e8eebf02ad350a3033a45c6d2fd
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2017-02-12 00:26:10 +0100

    working now

--- dentalAffinities/R/loadData.R
+++ dentalAffinities/R/loadData.R
@@ -8,9 +8,19 @@
 #' @param file xlsx file with data
 #'
 #' @return a data frame with data
+#' @importFrom openxlsx read.xlsx
 #' @export
 
 loadData <- function(file) {
+  file <- "~/GitHub/dentalAffinities/docs/test_file.xlsx"
+  dat <- read.xlsx(file, 1)
+  THRESHOLD <- dat[1,]
+  dat <- dat[-1,]
 
+  for (i in 4:ncol(dat)) {
+    dat[,i] <- (dat[,i] >= THRESHOLD[1,i]) + 0
+  }
+
+  dat
 }
 

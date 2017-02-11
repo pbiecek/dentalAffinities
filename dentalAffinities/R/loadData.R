6c605f49f7db752e60882904ad489cfb5bda4f36
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2017-02-12 00:19:42 +0100

    load data function

---
+++ dentalAffinities/R/loadData.R
@@ -0,0 +1,16 @@
+#' Read Data From Excel File
+#'
+#' First three collumns in the file should contain ID, SITE and SEX,
+#' following columns shall contain data in ordinal scale.
+#' Moreover it is assumed, that the second row is names THRESHOLD
+#' and contains cut off values.
+#'
+#' @param file xlsx file with data
+#'
+#' @return a data frame with data
+#' @export
+
+loadData <- function(file) {
+
+}
+

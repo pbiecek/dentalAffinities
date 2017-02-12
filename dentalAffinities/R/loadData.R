9ddb500c78cbe0a34d2cf5cdc87c2f34cac2f467
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2017-02-12 01:23:38 +0100

    now shiny

--- dentalAffinities/R/loadData.R
+++ dentalAffinities/R/loadData.R
@@ -1,8 +1,8 @@
 #' Read Data From Excel File
 #'
 #' First three collumns in the file should contain ID, SITE and SEX,
-#' following columns shall contain data in ordinal scale.
-#' Moreover it is assumed, that the second row is names THRESHOLD
+#' following columns shall contain trait scores in ordered scale.
+#' Moreover, it is assumed, that the second row is names THRESHOLD
 #' and contains cut off values.
 #'
 #' @param file xlsx file with data
---
+++ shiny2/ui.R
@@ -0,0 +1,87 @@
+library("shiny")
+library("rbokeh")
+library("htmlwidgets")
+
+# ciekawy artykul
+# http://www.pacea.u-bordeaux1.fr/IMG/pdf/AnthropMMD_v101_en.pdf
+# https://www.academia.edu/14001615/A_critical_review_of_the_Mean_Measure_of_Divergence_and_Mahalanobis_Distances_using_artificial_data_and_new_approaches_to_the_estimation_of_biodistances_employing_nonmetric_traits
+
+fluidPage(
+  fluidRow(class = "myRow1",
+    column(width = 4, h2("Dental Affinities"), p("Link to the article, contact email")),
+    column(width = 4,
+           fileInput('file1', 'Or upload your data in XLSX file',
+                     accept=c('.csv', 'application/xlsx',
+                              'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+                              '.xlsx')),
+           br(),
+           checkboxInput("button",label = "Example data")
+        ),
+    column(width = 2,
+           p("Options:"),
+           selectInput("init_trait", label = "Initial trait selection",
+                       choices = c("All traits" = "ALL",
+                                   "Only right side" = "RIGHT",
+                                   "Only left side" = "LEFT",
+                                   "Maximum score" = "MAX",
+                                   "Minimum score" = "MIN",
+                                   "Average score" = "AVE"
+                       ),
+                       selected = "ALL"),
+           selectInput("sex_handling", label = "Sex handling",
+                       choices = c("All individuals" = "ALL",
+                                   "Males only" = "MALE",
+                                   "Females only" = "FEMALE",
+                                   "Sample-wise selection" = "SAMPLE",
+                                   "Predefined selection" = "PRE"
+                       ),
+                       selected = "ALL"),
+           selectInput("post_trait", label = "Post-hoc trait selection",
+                       choices = c("All traits" = "ALL",
+                                   "Traits that differentiate" = "DIFF",
+                                   "Traits with high inter-sample variance" = "VAR"
+                       ),
+                       selected = "ALL")),
+    column(width = 2,
+           p("Options:"),
+           selectInput("method_sel", label = "Method selection",
+                       choices = c("MMD - Anscombe" = "MMD_ANS",
+                                   "MMD - Anscombe (Freeman & Tukey correction)" = "MMD_ANS_FRE",
+                                   "MMD - Anscombe (Grewal correction)" = "MMD_ANS_GRE",
+                                   "MMD - Freeman & Tukey" = "MMD_FRE",
+                                   "MMD - Freeman & Tukey (Freeman & Tukey correction)" = "MMD_FRE_FRE",
+                                   "MMD - Freeman & Tukey (Grewal correction)" = "MMD_FRE_GRE",
+                                   "Mahalanobis - tetrachoric correlation (TMD)" = "MAH_TMD",
+                                   "Mahalanobis - ordinal (OMD)" = "MAH_OMD",
+                                   "Mahalanobis - corrected ordinal (COMD)" = "MAH_COMD",
+                                   "Mahalanobis - Pearson correlation coefficients (RMD)" = "MAH_RMD",
+                                   "PCA - polychoric correlation " = "MAH_PCA"
+                       ),
+                       selected = "MMD_ANS"),
+           selectInput("binarisation", label = "Binarization",
+                       choices = c("User defined" = "USER",
+                                   "Balanced" = "BALANCED",
+                                   "Highest inter sample" = "HIGH"
+                       ),
+                       selected = "BALANCED"),
+           selectInput("measure", label = "Select distance measure",
+                       choices = c("Mean measure of divergence" = "MMD"
+                                   #"Tetrachoric Mahalanobis D2 distance" = "TMD"
+                       ),
+                       selected = "MMD"),
+          selectInput("transforamtion", label = "How to transform values",
+                      choices = c("Anscombe transformation" = "ATR",
+                                  "Freeman-Tukey transformation" = "FTT"),
+                      selected = "ATR"))
+    ),
+  fluidRow(
+    column(width = 4, plotOutput('ggMDS')),
+    column(width = 4, plotOutput('ggClust')),
+    column(width = 4,  verbatimTextOutput('textSummary'))
+  ),
+  tags$head(tags$style("
+      .myRow1{background-color: #dddddd;}
+      .myRow3{height:3px; background-color: #dddddd;}
+     "
+  ))
+)

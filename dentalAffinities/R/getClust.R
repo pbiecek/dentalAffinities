f16a8f77d4f82e5c5d2953476285702e35808de0
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2017-02-12 15:08:24 +0100

    use ggdendro

--- dentalAffinities/R/getClust.R
+++ dentalAffinities/R/getClust.R
@@ -3,11 +3,13 @@
 #' @param mat distance matrix
 #'
 #' @importFrom cluster agnes
+#' @importFrom stats as.dendrogram
+#' @importFrom ggdendro ggdendrogram
 #' @import ggplot2
 #' @return ggplot2 plot
 #' @export
 getClust <- function(mat) {
   grupy <- agnes(as.dist(mat), method = "ward")
-  fviz_dend(grupy, rect = TRUE, main = "Clustering: Ward method") +
-    scale_y_continuous(expand = c(0.2,0))
+  dg <- as.dendrogram(grupy)
+  ggdendrogram(dg)
 }

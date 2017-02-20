ebe42ad95f7b0644ab44ce2ae8cdcd419be8658d
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2017-02-20 03:57:44 +0100

    better scaling

--- dentalAffinities/R/getMDS.R
+++ dentalAffinities/R/getMDS.R
@@ -15,10 +15,11 @@ getMDS <- function(mat, metric = TRUE) {
     np <- isoMDS(as.dist(mat), k = 2)$points
   }
   df <- data.frame(site = rownames(np), x = np[,1], y = np[,2])
+  xlim <- c(min(c(df[,2], df[,3]), na.rm=TRUE), max(c(df[,2], df[,3]), na.rm=TRUE))
   ggplot(df, aes(x,y)) +
     geom_point(size = 2) +
-    scale_x_continuous(expand = c(0.05,0.05)) +
-    scale_y_continuous(expand = c(0.05,0.05)) +
+    scale_x_continuous(expand = c(0.05,0.05), limits = xlim) +
+    scale_y_continuous(expand = c(0.05,0.05), limits = xlim) +
     coord_fixed() +
     geom_text(aes(label = site), vjust = -0.3) +
     xlab("") + ylab("") + theme_classic() +

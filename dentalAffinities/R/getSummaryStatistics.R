ffe495544314841da088a89896b5a8697d34d4a4
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2017-02-20 02:26:07 +0100

    new function

---
+++ dentalAffinities/R/getSummaryStatistics.R
@@ -0,0 +1,16 @@
+#' Create a summary statistics
+#'
+#' @param df dataframe with variables
+#'
+#' @return data frame with summary statistics
+#' @import tidyr
+#' @import dplyr
+#' @export
+getSummaryStatistics <- function(df) {
+  tmp <- df[,-(1:3)]
+  res <- na.omit(gather(tmp, var, val))
+  ndf <- res %>% group_by(var, val) %>% summarise(n = n()) %>% spread(val, n, fill = 0)
+  ndf <- as.data.frame(ndf)
+  colnames(ndf)[1] <- "Frequency of values"
+  ndf
+}
